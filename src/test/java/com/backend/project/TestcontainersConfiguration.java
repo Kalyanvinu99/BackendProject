60b4b45a38e8c257622f7cdc1bbd8a360a34872b
Kalyan <kalyanvinu99@gmail.com>
2024-09-21 13:14:21 +0530

    Backend Project First Commit

---
+++ src/test/java/com/backend/project/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.backend.project;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
